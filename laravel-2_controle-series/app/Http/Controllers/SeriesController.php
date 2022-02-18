b2689a1f6dbc462f39ada3b9eb3dfc4a404d2c68
waldyr <waldyr.turquetti@escala-app.com>
2022-02-18 01:59:59 -0300

    feat: Finalizado o Capítulo 2.

--- laravel-2_controle-series/app/Http/Controllers/SeriesController.php
+++ laravel-2_controle-series/app/Http/Controllers/SeriesController.php
@@ -8,51 +8,11 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-
-//    public function index(Request $request){
-//
-//        echo $request->url();
-//        echo "</br>";
-//        echo $request->query('teste');
-//        echo "</br>";
-//        var_dump($request->query());
-//
-//
-//        $series = [
-//            'Arcane',
-//            'Lost',
-//            'Loky'
-//        ];
-//
-//        $html = "<ul>";
-//        foreach ($series as $serie){
-//            $html .= "<li>$serie</li>";
-//        }
-//        $html .= "</ul>";
-//
-//        return $html;
-//    }
-
     public function index(Request $request){
-
-//        $series = [
-//            'Arcane',
-//            'Mr. Robot',
-//            'Loky'
-//        ];
-
-//        $series = Serie::all();
 
         $series = Serie::query()->orderBy('nome')->get();
 
-//        return view('series.index', [
-//            'series' => $series
-//        ]);
-
         $mensagem = $request->session()->get('mensagem');
-//        $request->session()->remove('mensagem');
-
-//        return view('series.index', compact('series'));
 
         return view('series.index', compact('series', 'mensagem'));
     }
@@ -62,37 +22,23 @@ class SeriesController extends Controller
         return view('series.create');
     }
 
-//    public function store(Request $request)
     public function store(SeriesFormRequest $request)
     {
-//        $nome = $request->nome;
-//        var_dump($nome);
+        $serie = Serie::create(['nome' => $request->nome]);
 
-//        $serie = new Serie();
-//        $serie->nome = $nome;
-//        var_dump($serie->save());
+        $qtdTemporadas = $request->qtd_temporadas;
 
-//        var_dump(Serie::create([
-//            'nome' => $nome
-//        ]));
+        for($i = 1; $i <= $qtdTemporadas; $i++){
+           $temporada = $serie->temporadas()->create(['numero' => $i]);
 
-//        $serie = Serie::create([
-//            'nome' => $nome
-//        ]);
-
-//        $request->validate([
-//            'nome' => 'required|min:3'
-//        ]);
-
-        $serie = Serie::create($request->all());
-
-//        echo "Série com id ($serie->id) criada: $serie->nome";
+           for ($j = 1; $j <= $request->ep_por_temporada; $j++){
+               $temporada->episodios()->create(['numero' => $j]);
+           }
+        }
 
         $request->session()
-//            ->put('mensagem', "Série ($serie->id) criada com sucesso $serie->nome" );
-            ->flash('mensagem', "Série ($serie->id) criada com sucesso $serie->nome" ); //Flash message
+            ->flash('mensagem', "Série ($serie->nome) e suas temporadas e episodios criados com sucesso $serie->nome" ); //Flash message
 
-//        return redirect('/series');
         return redirect()->route('listar_series');
     }
 
